657e5353f6c95a0b6e58a20d1b528b4094990011
djfemz <oladejifemi00@gmail.com>
2025-09-01 10:51:09 -0500

    initial commit

---
+++ src/main/java/dev/litebank/security/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package dev.litebank.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return null;
+    }
+
+}
---
+++ src/main/java/dev/litebank/security/dto/request/AuthRequest.java
@@ -0,0 +1,11 @@
+package dev.litebank.security.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthRequest {
+    private String username;
+    private String password;
+}
